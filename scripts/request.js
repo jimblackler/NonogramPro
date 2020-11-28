ba68ffba80f494befe26f4143f9e787066ecd4ad
Jim Blackler <jimblackler@google.com>
2020-11-28 22:39:52 +0000

    Delete more duplicate files.

--- scripts/request.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function request(url, method, data, onloadend) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url, true);
-  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  xhr.onloadend = onloadend;
-  xhr.send(JSON.stringify(data));
-}
