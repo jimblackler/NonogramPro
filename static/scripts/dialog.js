60d3aecfff41c90746b965b7dae5fbbf5ecb2890
Jim Blackler <jimblackler@google.com>
2020-11-28 23:08:15 +0000

    Minor update.

--- static/scripts/dialog.js
+++ static/scripts/dialog.js
@@ -3,8 +3,7 @@
 (function () {
 
   /* Dialog */
-  const close_button = document.getElementById('close');
-  close_button.addEventListener('click', evt => {
+  document.getElementById('close').addEventListener('click', evt => {
     document.getElementById('dialog').style.visibility = 'hidden';
   });
   makeDraggable(document.getElementById('dialog'));
