32326f9ef9bcc87f6bbfacd6743433648bf814bc
Jim Blackler <jimblackler@google.com>
2022-10-05 17:11:08 +0100

    Use axios in getGamesInternet.

--- src/client/fetchGame.ts
+++ src/client/fetchGame.ts
@@ -1,28 +1,25 @@
+import axios from 'axios';
 import {ClientGameData} from '../common/clientGame';
 import {GamesDb} from './db/gamesDb';
 import {decode} from './decoder';
-import {request} from './request';
 
 function getGameInternet(gamesDb: GamesDb, gameId: string,
                          resolve: (game: ClientGameData) => void, reject: () => void) {
-  request(`/games?id=${gameId}`, 'GET', {}, evt => {
-    const target = evt.target;
-    if (!(target instanceof XMLHttpRequest)) {
-      throw new Error();
-    }
-    const obj = JSON.parse(target.response);
-    if (obj.results.length === 1) {
-      const game = obj.results[0].data as ClientGameData;
-      if (typeof game.grid_data !== 'string') {
-        throw new Error();
-      }
-      game.grid_data = decode(game.spec, game.grid_data);
-      game.needs_publish = false;
-      gamesDb.set(gameId, game).then(() => resolve(game));
-    } else {
-      reject();
-    }
-  });
+  axios.get(`/games?id=${gameId}`)
+      .then(value => value.data)
+      .then(obj => {
+        if (obj.results.length === 1) {
+          const game = obj.results[0].data as ClientGameData;
+          if (typeof game.grid_data !== 'string') {
+            throw new Error();
+          }
+          game.grid_data = decode(game.spec, game.grid_data);
+          game.needs_publish = false;
+          gamesDb.set(gameId, game).then(() => resolve(game));
+        } else {
+          reject();
+        }
+      });
 }
 
 export function getGame(gamesDb: GamesDb, gameId: string,
