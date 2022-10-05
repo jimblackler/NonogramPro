abc49c720673bcf7767ea778dba367532af93de9
Jim Blackler <jimblackler@google.com>
2022-10-05 12:48:49 +0100

    Name fixes.

--- src/client/play.ts
+++ src/client/play.ts
@@ -10,12 +10,12 @@ import {plotLine} from './plot_line';
 import {Renderer} from './renderer';
 
 class Play {
-  private readonly games_db: GamesDb;
-  private readonly plays_db: PlaysDb;
-  private readonly completed_db: CompletedDb;
-  private readonly game_id: string;
-  private row_lock: number | false = false;
-  private column_lock: number | false = false;
+  private readonly gamesDb: GamesDb;
+  private readonly playsDb: PlaysDb;
+  private readonly completedDb: CompletedDb;
+  private readonly gameId: string;
+  private rowLock: number | false = false;
+  private columnLock: number | false = false;
   private spec: Spec = {width: 0, height: 0};
   private clues: number[][][] = [];
   private data: boolean[][] = [];
@@ -23,23 +23,23 @@ class Play {
   private renderer: Renderer | undefined;
   private on: boolean[][] = [];
   private off: boolean[][] = [];
-  private last_x = -1;
-  private last_y = -1;
+  private lastX = -1;
+  private lastY = -1;
 
   constructor() {
-    this.games_db = new GamesDb();
-    this.plays_db = new PlaysDb();
-    this.completed_db = new CompletedDb();
-    this.row_lock = false;
-    this.column_lock = false;
-    this.game_id = new URL(window.location.href).searchParams.get('game') || '';
+    this.gamesDb = new GamesDb();
+    this.playsDb = new PlaysDb();
+    this.completedDb = new CompletedDb();
+    this.rowLock = false;
+    this.columnLock = false;
+    this.gameId = new URL(window.location.href).searchParams.get('game') || '';
 
     const svg = document.getElementsByTagName('svg')[0];
     if (!(svg instanceof SVGSVGElement)) {
       throw new Error();
     }
     getGame(
-        this.games_db, this.game_id,
+        this.gamesDb, this.gameId,
         result => {
           if (typeof result.grid_data !== 'object') {
             throw new Error();
@@ -50,11 +50,11 @@ class Play {
           this.on = Generate.getEmpty(this.spec);
           this.off = Generate.getEmpty(this.spec);
           this.renderer = new Renderer(svg, this.spec);
-          const color_scheme_stylesheet = document.getElementById('color_scheme_stylesheet');
-          if (!(color_scheme_stylesheet instanceof HTMLLinkElement)) {
+          const colorSchemeStylesheet = document.getElementById('color_scheme_stylesheet');
+          if (!(colorSchemeStylesheet instanceof HTMLLinkElement)) {
             throw new Error();
           }
-          color_scheme_stylesheet.href = `/styles/color_schemes/${this.style}.css`;
+          colorSchemeStylesheet.href = `/styles/color_schemes/${this.style}.css`;
           const title = document.getElementById('title');
           if (!title) {
             throw new Error();
@@ -62,7 +62,7 @@ class Play {
           title.textContent = result.name;
           this.clues = generateClues(this.spec, this.data);
           this.renderer.paintClues(this.clues);
-          this.plays_db.get(this.game_id).then(data => {
+          this.playsDb.get(this.gameId).then(data => {
             if (!data) {
               return;
             }
@@ -91,7 +91,7 @@ class Play {
     });
 
     edit.addEventListener('click', () => {
-      window.location.href = `edit?game=${this.game_id}`;
+      window.location.href = `edit?game=${this.gameId}`;
     });
 
     hint.addEventListener('click', () => {
@@ -111,7 +111,7 @@ class Play {
       SET_UNKNOWN: 3,
     };
 
-    let action_mode = ActionMode.NOT_DRAWING;
+    let actionMode = ActionMode.NOT_DRAWING;
 
     svg.addEventListener('contextmenu', evt => {
       evt.preventDefault();
@@ -126,13 +126,13 @@ class Play {
           if (which === 3 || shiftKey) {
             // Right click.
             if (this.off[y][x]) {
-              action_mode = ActionMode.SET_UNKNOWN;
+              actionMode = ActionMode.SET_UNKNOWN;
               this.on[y][x] = false;
               this.off[y][x] = false;
               this.checkColumn(x);
               this.checkRow(y);
             } else if (!this.off[y][x]) {
-              action_mode = ActionMode.SETTING_OFF;
+              actionMode = ActionMode.SETTING_OFF;
               this.on[y][x] = false;
               this.off[y][x] = true;
               this.checkColumn(x);
@@ -140,21 +140,21 @@ class Play {
             }
           } else {
             if (this.on[y][x]) {
-              action_mode = ActionMode.SET_UNKNOWN;
+              actionMode = ActionMode.SET_UNKNOWN;
               this.on[y][x] = false;
               this.off[y][x] = false;
               this.checkColumn(x);
               this.checkRow(y);
             } else if (!this.on[y][x]) {
-              action_mode = ActionMode.SETTING_ON;
+              actionMode = ActionMode.SETTING_ON;
               this.on[y][x] = true;
               this.off[y][x] = false;
               this.checkColumn(x);
               this.checkRow(y);
             }
           }
-          this.last_x = x;
-          this.last_y = y;
+          this.lastX = x;
+          this.lastY = y;
           this.repaint()
         }
       });
@@ -168,70 +168,70 @@ class Play {
         if (!this.renderer) {
           throw new Error();
         }
-        if (action_mode !== ActionMode.NOT_DRAWING && x >= 0 &&
+        if (actionMode !== ActionMode.NOT_DRAWING && x >= 0 &&
             x < this.spec.width && y >= 0 && y < this.spec.height) {
-          if (this.row_lock === false && this.column_lock === false) {
-            if (this.last_y !== y) {
-              this.column_lock = x;
+          if (this.rowLock === false && this.columnLock === false) {
+            if (this.lastY !== y) {
+              this.columnLock = x;
               this.renderer.setHighlightMode('locked');
               renderer.setHighlightColumn(x);
               renderer.setHighlightRow(-1);
-            } else if (this.last_x !== x) {
-              this.row_lock = y;
+            } else if (this.lastX !== x) {
+              this.rowLock = y;
               this.renderer.setHighlightMode('locked');
               renderer.setHighlightColumn(-1);
               renderer.setHighlightRow(y);
             }
           }
-          if (this.column_lock !== false) {
-            x = this.column_lock;
-          } else if (this.row_lock !== false) {
-            y = this.row_lock;
+          if (this.columnLock !== false) {
+            x = this.columnLock;
+          } else if (this.rowLock !== false) {
+            y = this.rowLock;
           }
 
-          let column_modified = new Set<number>();
-          let row_modified = new Set<number>();
+          let columnModified = new Set<number>();
+          let rowModified = new Set<number>();
           let modified = false;
-          for (let p of plotLine(this.last_x, this.last_y, x, y)) {
-            if (action_mode === ActionMode.SETTING_ON) {
+          for (let p of plotLine(this.lastX, this.lastY, x, y)) {
+            if (actionMode === ActionMode.SETTING_ON) {
               if (!this.on[p.y][p.x]) {
                 this.on[p.y][p.x] = true;
                 this.off[p.y][p.x] = false;
                 modified = true;
-                column_modified.add(p.x);
-                row_modified.add(p.y);
+                columnModified.add(p.x);
+                rowModified.add(p.y);
               }
-            } else if (action_mode === ActionMode.SETTING_OFF) {
+            } else if (actionMode === ActionMode.SETTING_OFF) {
               if (!this.off[p.y][p.x]) {
                 this.on[p.y][p.x] = false;
                 this.off[p.y][p.x] = true;
                 modified = true;
-                column_modified.add(p.x);
-                row_modified.add(p.y);
+                columnModified.add(p.x);
+                rowModified.add(p.y);
               }
-            } else if (action_mode === ActionMode.SET_UNKNOWN) {
+            } else if (actionMode === ActionMode.SET_UNKNOWN) {
               if (this.on[p.y][p.x] || this.off[p.y][p.x]) {
                 this.on[p.y][p.x] = false;
                 this.off[p.y][p.x] = false;
                 modified = true;
-                column_modified.add(p.x);
-                row_modified.add(p.y);
+                columnModified.add(p.x);
+                rowModified.add(p.y);
               }
             }
           }
-          this.last_x = x;
-          this.last_y = y;
+          this.lastX = x;
+          this.lastY = y;
           if (modified) {
-            for (let column of column_modified) {
+            for (let column of columnModified) {
               this.checkColumn(column);
             }
-            for (let row of row_modified) {
+            for (let row of rowModified) {
               this.checkRow(row);
             }
             this.repaint();
           }
         }
-        if (this.row_lock === false && this.column_lock === false) {
+        if (this.rowLock === false && this.columnLock === false) {
           if (x >= 0 && x < this.spec.width) {
             this.renderer.setHighlightMode('hover');
             renderer.setHighlightColumn(x);
@@ -248,18 +248,18 @@ class Play {
       if (!this.renderer) {
         throw new Error();
       }
-      action_mode = ActionMode.NOT_DRAWING;
-      this.row_lock = false;
-      this.column_lock = false;
+      actionMode = ActionMode.NOT_DRAWING;
+      this.rowLock = false;
+      this.columnLock = false;
 
       this.renderer.setHighlightMode('hover');
-      if (this.last_x !== undefined) {
-        this.renderer.setHighlightColumn(this.last_x);
+      if (this.lastX !== undefined) {
+        this.renderer.setHighlightColumn(this.lastX);
       }
 
       this.renderer.setHighlightMode('hover');
-      if (this.last_y !== undefined) {
-        this.renderer.setHighlightRow(this.last_y);
+      if (this.lastY !== undefined) {
+        this.renderer.setHighlightRow(this.lastY);
       }
     });
   }
@@ -300,13 +300,13 @@ class Play {
     if (!(svg instanceof SVGSVGElement)) {
       throw new Error();
     }
-    this.plays_db.set(this.game_id, {on: this.on, off: this.off});
+    this.playsDb.set(this.gameId, {on: this.on, off: this.off});
     this.renderer.paintOnSquares(this.on);
     this.renderer.paintOffSquares(this.off);
     /* Check is complete */
     if (Generate.equals(this.on, this.data)) {
       svg.classList.add('game_complete');
-      this.completed_db.set(this.game_id, {});
+      this.completedDb.set(this.gameId, {});
     } else {
       svg.classList.remove('game_complete');
     }
