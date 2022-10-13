77f2b3b74e2abdbee7a7acc915b48ea27837e18f
Jim Blackler <jimblackler@google.com>
2022-10-13 15:41:23 +0100

    Minor simplification.

--- src/client/db/gamesDb.ts
+++ src/client/db/gamesDb.ts
@@ -1,16 +1,11 @@
-import {truthy} from '../truthy';
-
 export const gamesDb: Promise<IDBDatabase> = new Promise((resolve, reject) => {
   const request = indexedDB.open('games-store', 9);
   request.addEventListener('upgradeneeded', event => {
-    let objectStore;
     if (event.oldVersion < 9) {
       if (request.result.objectStoreNames.contains('games')) {
         request.result.deleteObjectStore('games');
       }
-      objectStore = request.result.createObjectStore('games');
-      objectStore = truthy(request.transaction).objectStore('games');
-      objectStore.createIndex('byDifficulty', 'difficulty');
+      request.result.createObjectStore('games').createIndex('byDifficulty', 'difficulty');
     }
   });
   request.addEventListener('error', () => reject(request.error));
