5078dd5030eb45dc01bb05bc3a688cb20a890f60
Jim Blackler <jimblackler@google.com>
2022-10-13 22:36:12 +0100

    Completed games list, plus reworked list DOM.

--- src/client/listMain.ts
+++ src/client/listMain.ts
@@ -1,64 +1,67 @@
 import axios from 'axios';
 import {ClientGameData} from '../common/clientGame';
+import {completedDb} from './db/completedDb';
 import {gamesDb} from './db/gamesDb';
 import {playsDb} from './db/playsDb';
 import {decode} from './decoder';
 import {notNull} from './notNull';
 import {requestToAsyncGenerator} from './requestToAsyncGenerator';
 
-function addGame(key: string, game: ClientGameData, playing: boolean, list: HTMLElement) {
+function addGame(
+    key: string, game: ClientGameData, playing: boolean, completed: boolean, list: HTMLElement) {
   const li = document.createElement('li');
-  list.appendChild(li);
+  list.append(li);
 
   const anchor = document.createElement('a');
-  li.appendChild(anchor);
+  li.append(anchor);
   anchor.setAttribute('href', `/play?game=${key}`);
 
   /* Name */
   {
     const name = document.createElement('span');
-    name.classList.add('name');
-    name.appendChild(document.createTextNode(game.name));
-    anchor.appendChild(name);
+    anchor.append(name);
+    name.setAttribute('class', 'name');
+    name.append(game.name);
   }
 
   if (game.creator) {
     const creator = document.createElement('span');
-    creator.classList.add('creator');
-    creator.appendChild(document.createTextNode(`by ${game.creator}`));
-    anchor.appendChild(creator);
+    anchor.append(creator);
+    creator.setAttribute('class', 'creator');
+    creator.append(`by ${game.creator}`);
   }
 
   if (game.difficulty) {
     const name = document.createElement('span');
-    name.classList.add('difficulty');
-    const text = document.createTextNode(`Difficulty ${game.difficulty}`);
-    name.appendChild(text);
-    anchor.appendChild(name);
+    anchor.append(name);
+    name.setAttribute('class', 'difficulty');
+    name.append(`Difficulty ${game.difficulty}`);
   }
 
   if (game.needsPublish) {
     const name = document.createElement('span');
-    name.classList.add('draft');
-    name.appendChild(document.createTextNode('Draft'));
-    anchor.appendChild(name);
+    anchor.append(name);
+    name.append('Draft');
   }
 
   /* Dimensions */
   {
     const dimensions = document.createElement('span');
-    dimensions.classList.add('dimensions');
-    const text =
-        document.createTextNode(`${game.spec.width} x ${game.spec.height}`);
-    dimensions.appendChild(text);
-    anchor.appendChild(dimensions);
+    anchor.append(dimensions);
+    dimensions.setAttribute('class', 'dimensions');
+    dimensions.append(`${game.spec.width} x ${game.spec.height}`);
   }
 
-  if (playing) {
-    const playing = document.createElement('span');
-    playing.classList.add('playing');
-    playing.appendChild(document.createTextNode('In progress'));
-    anchor.appendChild(playing);
+  if (completed) {
+    const span = document.createElement('span');
+    anchor.append(span);
+    span.setAttribute('class', 'completed');
+    span.append('Completed');
+  } else if (playing) {
+    const span = document.createElement('span');
+    anchor.append(span);
+    span.setAttribute('class', 'playing');
+    span.append('In progress');
   }
 }
 
@@ -75,13 +78,20 @@ async function populate() {
     plays.add(currentTarget.key.toString());
   }
 
+  const completed = new Set<string>();
+  for await (const currentTarget of await completedDb
+      .then(db => db.transaction('completed', 'readonly').objectStore('completed').openCursor())
+      .then(requestToAsyncGenerator)) {
+    completed.add(currentTarget.key.toString());
+  }
+
   if ((new URL(window.location.href).searchParams.get('v') || 'local') === 'local') {
     for await (const result of await gamesDb
         .then(db => db.transaction('games', 'readonly')
             .objectStore('games').index('byDifficulty').openCursor())
         .then(requestToAsyncGenerator)) {
-      const primaryKey = result.primaryKey.toString();
-      addGame(primaryKey, result.value, plays.has(primaryKey), list);
+      const key = result.primaryKey.toString();
+      addGame(key, result.value, plays.has(key), completed.has(key), list);
     }
     progress.remove();
   } else {
@@ -89,7 +99,7 @@ async function populate() {
         .then(response => response.data)
         .then(obj => {
           for (let game of obj.results) {
-            addGame(game.key, game.data, plays.has(game.key), list);
+            addGame(game.key, game.data, plays.has(game.key), completed.has(game.key), list);
             // We write the incoming games to the local database (which needs
             // the grid decoding). Might not always be desirable.
             game.data.gridData = decode(game.data.spec, game.data.gridData);
