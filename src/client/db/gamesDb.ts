bca481d09ec175678e2f9a60de9c845d6548ea46
Jim Blackler <jimblackler@google.com>
2022-10-13 13:58:01 +0100

    Rethink versioning for games database.

--- src/client/db/gamesDb.ts
+++ src/client/db/gamesDb.ts
@@ -1,19 +1,17 @@
+import {truthy} from '../truthy';
+
 export const gamesDb: Promise<IDBDatabase> = new Promise((resolve, reject) => {
-  const request = indexedDB.open('games-store', 5);
+  const request = indexedDB.open('games-store', 9);
   request.addEventListener('upgradeneeded', event => {
     let objectStore;
-    if (event.oldVersion < 1) {
-      objectStore = request.result.createObjectStore('games');
-    }
-    if (event.oldVersion < 3) {
-      const transaction = request.transaction;
-      if (!transaction) {
-        throw new Error();
+    if (event.oldVersion < 9) {
+      if (request.result.objectStoreNames.contains('games')) {
+        request.result.deleteObjectStore('games');
       }
-      objectStore = transaction.objectStore('games');
+      objectStore = request.result.createObjectStore('games');
+      objectStore = truthy(request.transaction).objectStore('games');
       objectStore.createIndex('byDifficulty', 'difficulty');
     }
-    return objectStore;
   });
   request.addEventListener('error', () => reject(request.error));
   request.addEventListener('success', () => resolve(request.result));
