c2f467c20d6607e330524281d5f19aa079805202
Jim Blackler <jimblackler@google.com>
2022-10-17 10:16:11 +0100

    Removed unused code.

--- src/client/generate.ts
+++ src/client/generate.ts
@@ -36,34 +36,3 @@ export class Generate {
     });
   }
 }
-
-function generate(spec: Spec) {
-  const random = Alea();
-
-  const data = [];
-  for (let y = 0; y < spec.height; y++) {
-    const row = [];
-    for (let x = 0; x < spec.width; x++) {
-      row.push(random() < .40);
-    }
-    data.push(row);
-  }
-
-  for (let count = 0; count < 5000; count++) {
-    const x = random() * spec.width | 0;
-    const y = random() * spec.height | 0;
-    data[y][x] = true;
-    const mode = random() * 4 | 0;
-    if (mode === 0) {
-      data[y][x] = data[y === 0 ? spec.height - 1 : y - 1][x];
-    } else if (mode === 1) {
-      data[y][x] = data[y][x === spec.width - 1 ? 0 : x + 1];
-    } else if (mode === 2) {
-      data[y][x] = data[y === spec.height - 1 ? 0 : y + 1][x];
-    } else if (mode === 3) {
-      data[y][x] = data[y][x === 0 ? spec.width - 1 : x - 1];
-    }
-  }
-
-  return data;
-}
