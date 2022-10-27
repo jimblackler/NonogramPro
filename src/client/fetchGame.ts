0fc85c0848ae42f13f67ebdf9d8cee340aab67d5
Jim Blackler <jimblackler@google.com>
2022-10-27 22:28:22 +0100

    Look in main list of puzzles for puzzles.

--- src/client/fetchGame.ts
+++ src/client/fetchGame.ts
@@ -4,14 +4,18 @@ import {gamesDb} from './db/gamesDb';
 import {transactionToPromise} from './transactionToPromise';
 
 export function getGameInternet(gameId: string): Promise<GameData> {
-  return axios.get(`/games?id=${gameId}`)
+  // First we look in the main collection of games because this is virtually certain to be cached.
+  return axios.get(`/games?include=main`)
       .then(response => response.data as ClientGame[])
-      .then(games => {
-        if (games.length !== 1) {
-          throw new Error();
-        }
-        return games[0].data;
-      });
+      .then(games => games.find(game => game.key === gameId))
+      .then(game => game ? game.data : axios.get(`/games?id=${gameId}`)
+          .then(response => response.data as ClientGame[])
+          .then(games => {
+            if (games.length !== 1) {
+              throw new Error();
+            }
+            return games[0].data;
+          }));
 }
 
 export function getGame(gameId: string): Promise<GameData> {
