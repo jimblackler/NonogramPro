969682490619fbc07d8b6cb04dbba4b1f262728a
Jim Blackler <jimblackler@gmail.com>
2022-11-14 17:35:39 +0000

    Improved logging in assertion method.

--- src/common/check/is.ts
+++ src/common/check/is.ts
@@ -2,5 +2,5 @@ export function assertIs<T>(type: { new(...a: any[]): T }, object: any) {
   if (object instanceof type) {
     return object;
   }
-  throw new Error();
+  throw new Error(`Expected ${object} to be of type ${type}`);
 }
